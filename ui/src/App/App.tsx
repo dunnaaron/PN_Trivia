d3a6a526575e476183b1e28b782e6749041f4bed
Aaron Dunn <dunnac1@gmail.com>
2022-10-03 18:45:09 -0400

    implemented some base styling as examples moving forward. removed test file

---
+++ ui/src/App/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+import {
+	AppContainer,
+	ButtonContainer,
+	ButtonExample,
+	Logo,
+} from './App.styled';
+
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			<AppContainer className="App">
+				<Logo src="/logo-large.png" className="App-logo" alt="logo" />
+				<ButtonContainer>
+					<ButtonExample variant="contained">Example MUI Button</ButtonExample>
+				</ButtonContainer>
+			</AppContainer>
+		</ThemeProvider>
+	);
+}
+
+export default App;
